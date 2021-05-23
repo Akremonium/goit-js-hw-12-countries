2b24f875cd346807d17888fbf857d8b4a24a03f9
Akremonium <akremonium@gmail.com>
2021-05-23 20:29:23 +0300

    fixed

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,8 +1,7 @@
 export default function fetchCountries(searchQuery) {
   return fetch(`https://restcountries.eu/rest/v2/name/${searchQuery}`)
     .then(r => {
-      if (r.ok) return r.json();
-      throw new Error('Error fetching data');
+      return r.json();
     })
     .catch(error => {
       console.error('Error', error);
